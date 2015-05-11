f2236d4dc4de01e346bdbdb8a514516336d35686
Masaaki Morishita <ikasama3.0@gmail.com>
2015-05-11 21:58:37 +0900

    first implementation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = 
+module.exports = function plugin () {
+    return function (root) {
+        root.each(function (rule) {
+            if (checkBase(rule)) rule.removeSelf()
+        })
+    }
+}
+
+function checkBase (node) {
+    if (node.nodes) {
+        var children = node.nodes
+        var text = ''
+        children.forEach(function (child) {
+            if (child.type === 'comment') text = child.text
+        })
+        if (text.match(/\@base/)) return true
+    }
+    return false
+}
