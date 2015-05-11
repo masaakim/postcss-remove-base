a5f5f4c6c15b6720d13d9407b1c1da68d5a9b5b9
Masaaki Morishita <ikasama3.0@gmail.com>
2015-05-11 22:07:39 +0900

    add test code

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-var postcssRemoveBase = require('./')
+var removeBase = require('./')
+var fs = require('fs')
+var postcss = require('postcss')
+var test = require('tape')
+
+var css = fs.readFileSync('fixture.css', 'utf-8').trim()
+
+test('test', function (t) {
+    var actual = postcss().use(removeBase()).process(css).css.trim()
+    var expected = ".class {\n    padding: 10px;\n}"
+    t.same(actual, expected)
+    t.end()
+})
